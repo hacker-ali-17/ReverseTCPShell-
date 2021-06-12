7a256e063fcf834a2df264c8a736abe4c3886322
hacker ali 17 <64998647+hacker-ali-17@users.noreply.github.com>
2021-06-12 15:41:41 +0100

    Update ReverseTCP.ps1

--- ReverseTCP.ps1
+++ ReverseTCP.ps1
@@ -131,7 +131,7 @@ $Modules = @"
  | | \ \  __/\ V /  __/ |  \__ \  __/ | | |____| |        ____) | |  | |  __/ | |
  |_|  \_\___| \_/ \___|_|  |___/\___|_|  \_____|_|       |_____/|_|  |_|\___|_|_|
                                                      
-                                                                                     - By: @ZHacker13                                                                                                           
+                                                                                     - By: HACKER ALI 17                                                                                                           
 
  - | Modules    | - Show C2-Server Modules.
  - | Info       | - Show Remote-Host Info.
